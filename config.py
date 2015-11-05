f9f6050381dd84e9f53b80142151b77ac04ac199
Roger Howard <rogerhoward@gmail.com>
2015-11-04 23:01:49 -0800

    Disabled several config filesystems

--- config.py
+++ config.py
@@ -4,15 +4,17 @@ import requests, os, sys
 # path to the parent directory of the iiify.py application
 project_root = os.path.dirname(os.path.abspath(__file__))
 
-# path to where media is stored - make it if it doesn't exist
-media_root = os.path.join(project_root, 'media')
-if not os.path.exists(media_root):
-    os.makedirs(media_root)
+if False:
+    # path to where media is stored - make it if it doesn't exist
+    media_root = os.path.join(project_root, 'media')
+    if not os.path.exists(media_root):
+        os.makedirs(media_root)
 
-# path to where the disk cache is stored - make it if it doesn't exist
-cache_root = os.path.join(project_root, 'cache')
-if not os.path.exists(cache_root):
-    os.makedirs(cache_root)
+if False:
+    # path to where the disk cache is stored - make it if it doesn't exist
+    cache_root = os.path.join(project_root, 'cache')
+    if not os.path.exists(cache_root):
+        os.makedirs(cache_root)
 
 # path to where the disk cache is stored - make it if it doesn't exist
 static_root = os.path.join(project_root, 'static')
