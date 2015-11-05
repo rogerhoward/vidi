ad887cd254066b4223ba67b18221a06e84ee3dca
Roger Howard <rogerhoward@gmail.com>
2015-11-04 17:27:17 -0800

    Began command line options support

--- vidi.py
+++ vidi.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-import requests
+import requests, os, sys
+command_list = ['add', 'search']
+url_base = ''
 
 
 def add(path, id):
@@ -14,6 +16,7 @@ def add(path, id):
 
     return r.json()
 
+
 def find(path):
     url = 'http://localhost:8000/index/searcher'
 
@@ -26,16 +29,48 @@ def find(path):
 if __name__ == '__main__':
     import optparse
     parser = optparse.OptionParser()
+
+    parser.add_option('-i', '--input', dest='path', help='read image from FILE', metavar="FILE")
+    parser.add_option('-d', '--id', dest='id', help='index id', type=int)
+    parser.add_option('-c', '--command', dest='command', help='command mode')
+    parser.add_option('-p', '--port', dest='port', help='port number', type=int)
+    parser.add_option('-o', '--host', dest='host', help='hostname')
+
     options, args = parser.parse_args()
 
-    if args:
-        if args[0] == 'add':
-            input_path = args[1]
-            this_file_num = input_path[-8:][0:4]
-            print input_path, this_file_num
-            the_result = add(input_path, this_file_num)
-            print the_result
-        elif args[0] == 'find':
-            input_path = args[1]
-            the_result = find(input_path)
-            print the_result
+    if options.path is None:
+        print 'sorry, you must provide the -i option at minimum'
+        exit
+    elif os.path.exists(options.path) is False:
+        print 'sorry, the path provided does not exist'
+        exit
+    elif os.path.isfile(options.path) is False:
+        print 'sorry, the path provided is not a file'
+        exit
+
+
+    if options.command is None:
+        options.command = 'search'
+    elif options.command not in command_list:
+        print 'sorry, %s is not a supported command.' % (options.command)
+        print 'supported commands are: %s' % (', '.join(command_list))
+        exit
+
+    if options.port is None:
+        options.port = 8000
+
+    if options.host is None:
+        options.host = 'localhost'
+
+    url_base = 'http://%s:%s' % (options.host, options.port)
+
+    if options.command == 'search':
+        the_result = find(options.path)
+    elif options.command == 'add':
+        if options.id is None:
+            print 'Sorry, the add command requires the -d ID parameter'
+            sys.exit(0)
+        else:
+            the_result = add(options.path, options.id)
+
+    # print the_result
