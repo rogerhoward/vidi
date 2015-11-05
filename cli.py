d129c37895d798582ee5cca3ebbb37205995bcaf
Roger Howard <rogerhoward@gmail.com>
2015-11-04 21:32:54 -0800

    Nearly complete cli mode

--- cli.py
+++ cli.py
@@ -1,57 +1,67 @@
 #!/usr/bin/env python
 import requests, os, sys
 from vidi import Server
-from pastec.PastecLib import PastecConnection, PastecException
+
+command_list = ['add','search','ping', 'save', 'load', 'clear', 'bulk']
 
 if __name__ == '__main__':
-    server = PastecConnection(pastecHost='localhost', pastecPort=8001)
-    server.ping()
+    import optparse
+    parser = optparse.OptionParser()
 
-# if __name__ == '__main__':
-#     import optparse
-#     parser = optparse.OptionParser()
+    parser.add_option('-i', '--input', dest='path', help='read image from FILE', metavar="FILE")
+    parser.add_option('-d', '--id', dest='id', help='index id', type=int)
+    parser.add_option('-c', '--command', dest='command', help='command mode')
+    parser.add_option('-p', '--port', dest='port', help='port number', type=int)
+    parser.add_option('-o', '--host', dest='host', help='hostname')
 
-#     parser.add_option('-i', '--input', dest='path', help='read image from FILE', metavar="FILE")
-#     parser.add_option('-d', '--id', dest='id', help='index id', type=int)
-#     parser.add_option('-c', '--command', dest='command', help='command mode')
-#     parser.add_option('-p', '--port', dest='port', help='port number', type=int)
-#     parser.add_option('-o', '--host', dest='host', help='hostname')
+    options, args = parser.parse_args()
 
-#     options, args = parser.parse_args()
-
-#     if options.path is None:
-#         print 'sorry, you must provide the -i option at minimum'
-#         exit
-#     elif os.path.exists(options.path) is False:
-#         print 'sorry, the path provided does not exist'
-#         exit
-#     elif os.path.isfile(options.path) is False:
-#         print 'sorry, the path provided is not a file'
-#         exit
+    # if options.path is None:
+    #     print ('sorry, you must provide the -i option at minimum')
+        
+    # elif os.path.exists(options.path) is False:
+    #     print ('sorry, the path provided does not exist')
+        
+    # elif os.path.isfile(options.path) is False:
+    #     print ('sorry, the path provided is not a file')
+        
 
 
-#     if options.command is None:
-#         options.command = 'search'
-#     elif options.command not in command_list:
-#         print 'sorry, %s is not a supported command.' % (options.command)
-#         print 'supported commands are: %s' % (', '.join(command_list))
-#         exit
+    if options.command is None:
+        options.command = 'search'
+    elif options.command not in command_list:
+        print ('sorry, %s is not a supported command.' % (options.command))
+        print ('supported commands are: %s' % (', '.join(command_list)))
+        exit
 
-#     if options.port is None:
-#         options.port = 8000
+    if options.port is None:
+        options.port = 8001
 
-#     if options.host is None:
-#         options.host = 'localhost'
+    if options.host is None:
+        options.host = 'localhost'
 
-#     url_base = 'http://%s:%s' % (options.host, options.port)
+    server = Server(host=options.host, port=options.port)
 
-#     if options.command == 'search':
-#         the_result = find(options.path)
-#     elif options.command == 'add':
-#         if options.id is None:
-#             print 'Sorry, the add command requires the -d ID parameter'
-#             sys.exit(0)
-#         else:
-#             the_result = add(options.path, options.id)
+    if options.command == 'ping':
+        result = server.ping()
+    elif options.command == 'bulk':
+        if options.id is None:
+            options.id = 1
+        result = server.bulk(path=options.path, start=options.id)
+    elif options.command == 'save':
+        result = server.save(options.path)
+    elif options.command == 'load':
+        result = server.load(options.path)
+    elif options.command == 'clear':
+        result = server.clear()
+    elif options.command == 'search':
+        result = server.find(options.path)
+    elif options.command == 'add':
+        if options.id is None:
+            print ('Sorry, the add command requires the -d ID parameter')
+            sys.exit(0)
+        else:
+            result = server.add(options.path, options.id)
 
-#     # print the_result
+
+    print(result)
--- vidi.py
+++ vidi.py
@@ -6,8 +6,7 @@ from config import *
 
 
 class Server(object):
-    """A subclass of GenericDynamicImage which implements IIIF 2.0-compliant
-    behavior:
+    """A pastec server object:
 
     Attributes:
         image: a Pillow Image object.
@@ -17,40 +16,67 @@ class Server(object):
     commands = ['add', 'search']
 
     def __init__(self, host='locahost', port=8001 ):
-        """Return a IIIFImage object minimally initiated by *identifier*
-        with additional parameters required to generate dynamic derivatives."""
-
+        """Return a Server object."""
         self.host = host
         self.port = port
         self.url = 'http://%s:%s' % (self.host, self.port)
 
-
-    def __unicode__(self):
-        """Return image info dictionary based on identifier."""
-        return self.url
-
-
     def ping(self):
         url = '%s/' % (self.url)
-
-        r = requests.post(url, json={'type':'PING'})
-
+        print(url)
+        r = requests.post(url, data={'type':'PING'})
         return r.json()
 
     def add(self, path, id):
         url = '%s/index/images/%s' % (self.url, id)
-
-        with open(path) as this_file:
+        print(url)
+        with open(path, 'rb') as this_file:
             body_data = this_file.read()
-            r = requests.put(url, data=body_data, headers={'content-type':'image/jpeg'})
-
+        r = requests.put(url, data=body_data, headers={'content-type':'image/jpeg'})
         return r.json()
 
+    def clear(self):
+        url = '%s/index/io' % (self.url)
+        print(url)
+        body_data = {"type": "CLEAR"}
+        r = requests.post(url, json=body_data)
+        return r.json()
+
+    def bulk(self, path, start=1):
+        url = '%s/index/io' % (self.url)
+        print('url: %s' % url)
+
+        for directory, directories, files in os.walk(path):
+            for filename in files:
+                file_path = os.path.join(directory, filename)
+                if filename.endswith('.jpg'):
+                    self.add(file_path, start)
+                    start += 1
+
+
+
+        body_data = {"type": "WRITE", "index_path": path}
+        r = requests.post(url, json=body_data)
+        return r.json()
+
+    def save(self, path):
+        url = '%s/index/io' % (self.url)
+        print(url, path)
+        body_data = {"type": "WRITE", "index_path": path}
+        r = requests.post(url, json=body_data)
+        return r.json()
+
+    def load(self, path):
+        url = '%s/index/io' % (self.url)
+        print(url, path)
+        body_data = {"type": "LOAD", "index_path": path}
+        r = requests.post(url, json=body_data)
+        return r.json()
 
     def find(self, path):
         url = '%s/index/searcher' % (self.url)
 
-        with open(path) as this_file:
+        with open(path, 'rb') as this_file:
             body_data = this_file.read()
             r = requests.post(url, data=body_data, headers={'content-type':'image/jpeg'})
 
